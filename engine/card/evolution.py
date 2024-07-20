692d46f8777b500265c261a75f675cbb0f93dd26
Taras <taras.zhilenko@gmail.com>
2024-07-19 22:56:20 -0400

    assets

---
+++ engine/card/evolution.py
@@ -0,0 +1,16 @@
+
+class EvolutionCard:
+    def __init__(self, main_trait, short_trait, back):
+        self.main_trait  = main_trait
+        self.short_trait = short_trait
+        self.back = back
+        pass
+    
+    def render_back(self):
+        pass
+
+    def render_main(self):
+        pass
+
+    def render_short(self):
+        pass
