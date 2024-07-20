de49bcfb8b0150357183de9703e6c01868545c07
Taras <taras.zhilenko@gmail.com>
2024-07-20 13:20:20 -0400

    test area card

--- basic_test.py
+++ basic_test.py
@@ -30,7 +30,6 @@ class Testing(unittest.TestCase):
         board.cast_trait(board.evolution_deck.draw(), 'short', 1)
         board.cast_trait(board.evolution_deck.draw(), 'main', 1)
         board.cast_trait(board.evolution_deck.draw(), 'short', 1)
-        board.animal_dict[1].render(board.trait_dict).print()
         board.discard_trait(9)
         board.discard_trait(8)
         board.discard_trait(7)
@@ -51,7 +50,17 @@ class Testing(unittest.TestCase):
         expected = pd.read_csv('asset/test/1/board_render.csv')
         self.assertTrue((board.render().arr == expected.to_numpy()).all())
     
-    def test_boolean(self):
-        a = True
-        b = True
-        self.assertEqual(a, b)
+    def test_area(self):
+        from engine.area.card import SubArea, AreaCard
+        from engine.render import Render
+        s1 = SubArea('glaciers', 'without_traits', 1, 0, Render.from_string('*'),       Render.from_string('#'), 10, 20, 0)
+        s2 = SubArea('tundra',   '', 1, 0,           Render.from_string('*'),       Render.from_string('#'), 10, 20, 1)
+        s1.add_green()
+        s1.add_green()
+        c1 = AreaCard()
+        c1.add_subarea(s1)
+        c1.add_subarea(s2)
+        c1.render_front()
+        expected = pd.read_csv('asset/test/2/areacard_render.csv')
+        self.assertTrue((c1.render_front().arr == expected.to_numpy()).all())
+
