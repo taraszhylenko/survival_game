03e7163be34691891195bb2054312c6df4baf408
Taras <taras.zhilenko@gmail.com>
2024-07-20 10:27:58 -0400

    add countre

---
+++ engine/misc/counter.py
@@ -0,0 +1,17 @@
+from engine.render import Render
+
+class Counter:
+    def __init__(self, label, value):
+        self.label = label
+        self.value = value
+
+    def increment(self):
+        self.value += 1
+        assert 0 <= self.value < 10
+
+    def decrement(self):
+        self.value -= 1
+        assert 0 <= self.value < 10
+
+    def render(self):
+        return Render.from_string(f'{self.label}: {self.value}')
