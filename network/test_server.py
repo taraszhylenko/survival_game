14b780eec4db79974d456492b7a6e69bcb24586e
Taras <taras.zhilenko@gmail.com>
2024-09-08 17:15:07 -0400

    refactor as GameServer class

--- network/test_server.py
+++ network/test_server.py
@@ -7,44 +7,49 @@ import threading
 
 from manager import NetworkManager
 
+class GameServer:
+    def __init__(self, ip, port, buff_size, tag):
+        self.ip = ip
+        self.port = port
+        self.buff_size = buff_size
+        self.tag = tag
 
-def main(ip, port, buff_size, tag):
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind((ip, port))
-    server.listen(5)
-    print(f'[*] Listening on {ip}:{port}')
+    def run_server(self):
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.bind((self.ip, self.port))
+        server.listen(5)
+        print(f'[*] Listening on {self.ip}:{self.port}')
 
-    lock = threading.Lock()
-    connections = dict()
+        self.lock = threading.Lock()
+        self.connections = dict()
 
-    while True:
-        sock, address = server.accept()
-        print(f'[*] Accepted connection from {address[0]}: {address[1]}')
-        threading.Thread(target=handle_connection, args=(sock, buff_size, lock, connections, tag)).start()
+        while True:
+            sock, address = server.accept()
+            print(f'[*] Accepted connection from {address[0]}: {address[1]}')
+            threading.Thread(target=self.handle_connection, args=(sock, )).start()
 
-def handle_connection(sock, buff_size, lock, connections, tag):
-    try:
-        today_str = datetime.datetime.today().strftime("%Y%m%d")
-        nm = NetworkManager(sock, buff_size)
-        game_tag = nm.recv()
-        assert bool(re.compile(f'{today_str}.*{tag}').match(game_tag)), \
-            f"{game_tag} doesn't match expectation {today_str}.*{tag}"
-        if game_tag in connections:
-            print(f'[*] creating game with two players')
-            sock_old = connections.pop(game_tag)
-            handle_game(sock_old, sock, buff_size, lock)
-        else:
-            print(f'[*] adding first player with tag {game_tag}')
-            connections[game_tag] = sock
-    except Exception as e:
-        print(e)
+    def handle_connection(self, sock):
+        try:
+            today_str = datetime.datetime.today().strftime("%Y%m%d")
+            nm = NetworkManager(sock, self.buff_size)
+            game_tag = nm.recv()
+            assert bool(re.compile(f'{today_str}.*{self.tag}').match(game_tag)), \
+                f"{game_tag} doesn't match expectation {today_str}.*{self.tag}"
+            if game_tag in self.connections:
+                print(f'[*] creating game with two players')
+                sock_old = self.connections.pop(game_tag)
+                self.handle_game(sock_old, sock)
+            else:
+                print(f'[*] adding first player to game {game_tag}')
+                self.connections[game_tag] = sock
+        except Exception as e:
+            print(e)
 
-
-def handle_game(sock0, sock1, buff_size, lock):
-    nm0 = NetworkManager(sock0, buff_size)
-    nm1 = NetworkManager(sock1, buff_size)
-    nm0.send("Connected as player 0")
-    nm1.send("Connected as player 1")
+    def handle_game(self, sock0, sock1):
+        nm0 = NetworkManager(sock0, self.buff_size)
+        nm1 = NetworkManager(sock1, self.buff_size)
+        nm0.send("Connected as player 0")
+        nm1.send("Connected as player 1")
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -55,4 +60,4 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    main(args.ip, args.port, args.buff_size, args.tag)
+    GameServer(args.ip, args.port, args.buff_size, args.tag).run_server()
